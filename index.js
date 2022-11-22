1b1f1f4d9144d6f1e05673ba8c866db3857c577c
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-22 22:52:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+// multiply: is an equation whose multiplied results will equal 62
+let multiply = num1 * num2;
+
+// randowm: generates a random integer greater than 0
+const random = Math.floor(Math.random() *10 + 1);
+
+let num3 = 4;
+let num4 = 5;
+
+//mod: is an equation that calculates a remainder that is equal to 4
+let mod = num3 % num4;
+
+// max: will return 20 as the highest number in the set
+let max = Math.max(10, 20, 15, 8);
+console.log(max);
+
+
+
